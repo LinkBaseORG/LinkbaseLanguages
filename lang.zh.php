52ea7b5bf852ea6ebd10bc658ce7307ae22aa63b
LinkBaseORG <38041853+LinkBaseORG@users.noreply.github.com>
2018-04-09 15:15:01 +0200

    Chinese

---
+++ lang.zh.php
@@ -0,0 +1,338 @@
+<?php
+// Global
+$lang['site_error_backText']        = '背部';
+$lang['site_403_errorText']         = '抱歉，您無權訪問此頁面。';
+$lang['site_404_errorText']         = '哎呀！ 抱歉，無法找到此頁面。';
+$lang['site_500_errorText']         = '對不起，有一個內部錯誤。';
+// Navigation
+$lang['navigation_toggle']          = '切換導航';
+$lang['navigation_home']            = '家';
+// NavigationSub
+$lang['navigation_boards']          = '論壇';
+$lang['navigation_boards_all']      = '所有論壇';
+$lang['navigation_boards_noFraud']  = '沒有欺詐';
+$lang['navigation_boards_tor']      = '只 Tor';
+$lang['navigation_boards_i2p']      = '只 I2p';
+// Navigation
+$lang['navigation_blogs']           = '博客';
+$lang['navigation_shops']           = '商店';
+$lang['navigation_warez']           = '盜版軟件';
+$lang['navigation_useful']          = '有用';
+$lang['navigation_vpn']             = '虛擬專用網絡';
+$lang['navigation_hosting']         = '主機';
+$lang['navigation_addLink']         = '添加鏈接';
+$lang['navigation_last5Added']      = '顯示最近更改的鏈接 (飼料)';
+// NavigationSub
+$lang['navigation_information']                       = '信息';
+$lang['navigation_information_contact']               = '接觸';
+$lang['navigation_information_donate']                = '捐贈';
+$lang['navigation_information_pricing']               = '價格';
+$lang['navigation_information_linkUs']                = '鏈接我們';
+$lang['navigation_information_misc']                  = '其他';
+$lang['navigation_information_misc_changelog']        = '更新日誌';
+$lang['navigation_information_misc_api']              = 'API';
+$lang['navigation_information_misc_faq']              = '常見問題';
+$lang['navigation_information_misc_rssAdded']         = '飼料 [額外]';
+$lang['navigation_information_misc_rssUpdated']       = '飼料 [變]';
+$lang['navigation_information_misc_siteUptime']       = '狀態頁';
+// Breadcrumbs
+$lang['navigation_breadcrumb_home']       = '家';
+$lang['navigation_breadcrumb_noFraud']    = '只沒有欺詐';
+$lang['navigation_breadcrumb_tor']        = '只 TOR 論壇';
+$lang['navigation_breadcrumb_i2p']        = '只 I2P 論壇';
+$lang['navigation_breadcrumb_search']     = '搜索結果';
+$lang['navigation_breadcrumb_blogs']      = '安全和黑客博客';
+$lang['navigation_breadcrumb_shops']      = '商店';
+$lang['navigation_breadcrumb_warez']      = '盜版軟件';
+$lang['navigation_breadcrumb_useful']     = '有用的鏈接';
+$lang['navigation_breadcrumb_vpn']        = 'VPN 提供商';
+$lang['navigation_breadcrumb_hosting']    = '主機 提供商';
+$lang['navigation_breadcrumb_addLink']    = '添加一個新的鏈接';
+$lang['navigation_breadcrumb_reportLink'] = '死鏈接的報告';
+$lang['navigation_breadcrumb_contact']    = '聯繫我們';
+$lang['navigation_breadcrumb_donate']     = '捐贈';
+$lang['navigation_breadcrumb_pricing']    = '廣告空間價格計劃';
+$lang['navigation_breadcrumb_linkUs']     = '鏈接我們';
+$lang['navigation_breadcrumb_changelog']  = '版本 更新日誌';
+$lang['navigation_breadcrumb_faq']        = '常問問題';
+// Subnavigation
+$lang['subnavigation_lastDbUpdate']       = '上次數據庫更新';
+// Sidebar
+$lang['sidebar_searchPlaceholder']    = '搜索';
+$lang['sidebar_linkQueue']            = '鏈接隊列';
+$lang['sidebar_linkQueue_added']      = '額外';
+$lang['sidebar_linkQueue_reported']   = '報導';
+$lang['sidebar_statistics']           = '統計';
+$lang['sidebar_statistics_online']    = '線上';
+$lang['sidebar_statistics_offline']   = '離線';
+$lang['sidebar_statistics_languages'] = '語言';
+$lang['sidebar_statistics_total']     = '整體';
+$lang['sidebar_advertisement']        = '廣告';
+$lang['sidebar_version']              = '版本 4.1';
+$lang['sidebar_version_changelog']    = '更新日誌';
+// Link us site
+$lang['linkus_htmlCodeText']          = '獲取HTML代碼，將我們的橫幅嵌入您的網站。';
+$lang['linkus_bbcodeText']            = '獲取HTML代碼並在簽名中插入我們的橫幅，例如在論壇中（MyBB，vBulletin，...）';
+// Donate site
+$lang['donate_headText']              = '捐贈以保持這個網頁的活躍：';
+// Changelog site
+$lang['changelog_head']         = '更新日誌';
+$lang['changelog_committed']    = '已經發布了一個新版本';
+// Pricing site
+$lang['pricing_smallHead']      = '小';
+$lang['pricing_smallInclude']   = '獲得特色 "紅星"';
+$lang['pricing_mediumHead']     = '手段';
+$lang['pricing_highlight']      = '最受歡迎的計劃';
+$lang['pricing_mediumInclude']  = '468px * 60px (+r紅星)';
+$lang['pricing_largeHead']      = '偉大';
+$lang['pricing_largeInclude']   = '920px * 120px (+紅星)';
+$lang['pricing_perMonth']       = '每個月';
+$lang['pricing_contactUs']      = '聯繫我們';
+// Search Site
+$lang['search_nameText']                = '名稱';
+$lang['search_typeHead']                = '類型';
+$lang['search_resetText']               = '重置搜索';
+$lang['search_category']                = '類別';
+$lang['search_typeBoard']               = '論壇';
+$lang['search_typeBlog']                = '博客';
+$lang['search_typeWarez']               = '盜版軟件';
+$lang['search_typeVPN']                 = 'VPN';
+$lang['search_typeUseful']              = '有幫助';
+$lang['search_typeShop']                = '店';
+$lang['search_typeHosting']             = '主機';
+// Contact us Site
+$lang['contact_donateText']             = '捐贈以保持這個網頁的活躍：';
+$lang['contact_contactInformationText'] = '聯繫方式：';
+$lang['contact_twitterFollow']          = '結果';
+$lang['contact_warningHeader']          = '警告!';
+$lang['contact_warningText']            = '如果您想與我們聯繫，請<b>僅使用顯示的<b>頂部</b>聯繫信息</b>以防止偽造！';
+// Report Link Site
+$lang['report_headText']              = '報告死鏈接';
+$lang['report_sitename']              = '名稱';
+$lang['report_url']                   = '域';
+$lang['report_reason']                = '原因';
+$lang['report_reportBtn']             = '報告鏈接';
+$lang['report_success_title']         = '成功!';
+$lang['report_success_text']          = '報導了鏈接。 感謝您的幫助.';
+$lang['report_fail_title']            = '警告!';
+$lang['report_fail_text']             = '有一個錯誤。 請再試一次.';
+$lang['report_fail_DoubleEntry_text'] = '此頁面已被報告.';
+
+// Add Link Site
+$lang['add_url']                      = '域';
+$lang['add_tor']                      = 'TOR 域';
+$lang['add_i2p']                      = 'I2P 域';
+$lang['add_facebook']                 = 'Facebook';
+$lang['add_twitter']                  = 'Twitter';
+$lang['add_category']                 = '類別';
+$lang['add_languages']                = '語言';
+$lang['add_pleaseSelect']             = '請選擇';
+$lang['add_information']              = '信息';
+$lang['add_information_placeholder']  = '例如，備份域名，聯繫信息，...（最多200個字符）';
+$lang['add_addLink']                  = '添加鏈接';
+$lang['add_success_title']            = '成功!';
+$lang['add_success_text']             = '鏈接已添加。 感謝您的幫助.';
+$lang['add_fail_title']               = '錯誤!';
+$lang['add_fail_text']                = '發生錯誤。 請再試一次.';
+$lang['add_facebook_placeholder']     = '組名';
+// Hosting Site
+$lang['hosting_head']                 = '';
+$lang['hosting_service']              = '提供商';
+$lang['hosting_creditcard']           = '信用卡';
+$lang['hosting_locations']            = '地點';
+$lang['hosting_infoTextHead']         = '信息!';
+$lang['hosting_infoText']             = '諾頓探索隱藏在地下掩體深處的防彈主機世界
+在海上分離並通過網絡分佈。 發現對這些服務的威脅，包括殭屍網絡，惡意軟件，勒索軟件和黑市
+並學習如何保護自己 "The Most Dangerous Town on the Internet - Where Cybercrime Goes to Hide"';
+$lang['hosting_infoStartVideo']       = '開始視頻';
+// Footer
+$lang['footer_generatedInStart']      = '頁面在 ';
+$lang['footer_generatedInEnd']        = ' 秒。';
+// Language Category
+$lang['promoted']        = '&#10035;廣告';
+$lang['partner']         = '夥伴';
+$lang['al']              = '阿爾巴尼亞人';
+$lang['ar']              = '阿拉伯語';
+$lang['az']              = '阿塞拜疆';
+$lang['bs']              = '波斯尼亞';
+$lang['zh']              = '中國';
+$lang['hr']              = '克羅地亞';
+$lang['cz']              = '捷克';
+$lang['da']              = '丹麥';
+$lang['nl']              = '荷蘭人';
+$lang['en']              = '英語';
+$lang['fr']              = '法國';
+$lang['ge']              = '格魯吉亞';
+$lang['de']              = '德國';
+$lang['gr']              = '希臘語';
+$lang['id']              = '印度尼西亞';
+$lang['it']              = '意大利';
+$lang['jp']              = '日本';
+$lang['my']              = '馬來西亞';
+$lang['fas']             = '波斯語';
+$lang['pl']              = '波蘭語';
+$lang['pt']              = '葡萄牙';
+$lang['ro']              = '羅馬尼亞';
+$lang['ru']              = '俄';
+$lang['sr']              = '塞爾維亞';
+$lang['es']              = '西班牙語';
+$lang['th']              = '泰國';
+$lang['tr']              = '土耳其';
+$lang['ua']              = '烏克蘭';
+$lang['uz']              = '烏茲別克';
+$lang['vn']              = '越南';
+// Shop Category
+$lang['shops_ccv']              = 'Cvv /轉儲';
+$lang['shops_accounts']         = '帳號';
+$lang['shops_icq']              = 'ICQ';
+$lang['shops_drugs']            = '毒品';
+$lang['shops_rdp']              = 'Rdp / Smtp';
+$lang['shops_flood']            = '洪水';
+$lang['shops_exploit']          = '利用';
+$lang['shops_cccheck']          = 'CC 檢查';
+$lang['shops_iptv']             = 'IPTV';
+$lang['shops_other']            = '其他';
+// Useful Category
+$lang['useful_vuln']            = '漏洞';
+$lang['useful_unsorted']        = '未分類';
+$lang['useful_ffaddons']        = 'Firefox 插件';
+$lang['useful_cryptography']    = '加密';
+$lang['useful_scanner']         = '掃描器';
+$lang['useful_antimalware']     = '反惡意軟件';
+$lang['useful_linklist']        = '鏈接列表';
+$lang['useful_multiuploader']   = 'Multiuploader';
+$lang['useful_exchanger']       = '器';
+$lang['useful_trashmail']       = 'Trash Mail';
+$lang['useful_challenges']      = '挑戰';
+$lang['useful_securemail']      = 'Secure Mail';
+$lang['useful_cardsharing']     = 'Cardsharing';
+$lang['useful_toplist']         = 'Toplists';
+$lang['useful_nfo']             = 'NFO';
+$lang['useful_ddosprotect']     = 'DDoS 保護';
+$lang['useful_cryptocurrency']  = '加密貨幣';
+$lang['useful_documentation']   = '文件';
+$lang['useful_linkcrypter']     = 'Linkcrypter';
+$lang['useful_startpage']       = '主頁頁面';
+$lang['useful_telegramgrp']     = 'Telegram 組';
+$lang['useful_uptimechecker']   = '正常運行時間檢查器';
+$lang['useful_pastebin']        = 'Pastebin';
+$lang['useful_security']        = '安全';
+$lang['useful_hackgame']        = '劈-遊戲';
+$lang['useful_serials']         = '序列號';
+// VPN Category
+$lang['vpn_paidvpn']            = '付費 VPN';
+$lang['vpn_vpninfo']            = 'VPN 信息';
+$lang['vpn_freevpn']            = '免費 VPN';
+$lang['vpn_socks']              = 'Socks';
+$lang['vpn_webproxy']           = 'Webproxy';
+$lang['vpn_checker']            = '檢查';
+// Warez Category
+$lang['warez_boards']           = '論壇';
+$lang['warez_anime']            = '動漫/ Hentai /漫畫';
+$lang['warez_tvstream']         = '電視流';
+$lang['warez_ftp']              = 'FTP / FXP';
+$lang['warez_documentation']    = '文件';
+$lang['warez_ebook']            = '電子書';
+$lang['warez_xdcc']             = 'XDCC';
+$lang['warez_music']            = '音樂';
+$lang['warez_games']            = '遊戲';
+$lang['warez_movies']           = '電影';
+$lang['warez_mixed']            = '雜';
+$lang['warez_android']          = 'Android';
+$lang['warez_apple']            = 'Apple';
+$lang['warez_moviestreams']     = '電影流';
+$lang['warez_search']           = '搜索';
+$lang['warez_porn']             = '色情';
+$lang['warez_usenet']           = 'NZB / Usenet';
+$lang['warez_scripts']          = '腳本';
+$lang['warez_torrent']          = 'Torrent';
+// Board Redirection Site
+$lang['out_home']               = '家';
+$lang['out_report']             = '報告死鏈接';
+$lang['out_voteForUs']          = '為我們投票';
+$lang['out_vote']               = '聲音';
+$lang['out_closeFrame']         = '鉤 Frame';
+$lang['out_redirectingTo']      = '轉發到 ';
+$lang['out_redirectingIn']      = ' 在20秒內。';
+$lang['out_iframeText1']        = '您的瀏覽器無法顯示iframe:';
+$lang['out_iframeText2']        = '點擊此處顯示請求的頁面:';
+// FAQ site
+$lang['faq_head']               = 'Frequently Asked Questions';
+// Link
+$lang['faq_menu_link']                            = 'Link';
+$lang['faq_menu_link_question_linkStatusColors']  = 'What are the meanings of the different link status colors?';
+$lang['faq_menu_link_answer_linkStatusColors']    = '<span class="fa fa-circle" style="color:green"></span> <font color="black">online (nonfraud)</font></p>
+<p><span class="fa fa-circle" style="color:green"></span> <font color="lightgrey">online </font>(fraud)</p>
+<p><span class="fa fa-circle" style="color:orange"></span> maintenance / back online soon</p>
+<p><span class="fa fa-circle" style="color:red"></span> offline</p>
+<p><span class="fa fa-globe" style="color:green"></span> Tor</i></p>
+<p><span class="fa fa-star" style="color:red"></span><font color="red"> <b>name</b> </font> Advertisement<p>
+<p><span class="fa fa-star" style="color:red"></span> Partner or banner backlink</p>
+<p><span class="fa fa-star" style="color:green"></span> Partner or backlink';
+$lang['faq_menu_link_question_linkOffline']       = 'A link is offline';
+$lang['faq_menu_link_answer_linkOffline']         = 'If there is a link which seems to be offline, you have the ability to report this site.</p>
+<p>We will review your reported link and mark the site as offline';
+$lang['faq_menu_link_question_addedLink']         = 'I submitted a link, but it is still not visible';
+$lang['faq_menu_link_answer_addedLink']           = 'We verify every submitted link within one week.</p>
+<p>If its still not visible, send us a DM via Twitter or contact us via jabber/icq.';
+$lang['faq_menu_link_question_socialData']       = 'How to get social data?';
+$lang['faq_menu_link_answer_socialData']         = 'If you open a forum link, you may get additional information in the headers bar.</p>
+<p>Complete informations you can get on the <a href="search.php">search</a> site.';
+// Privacy
+$lang['faq_menu_privacy']                            = 'Privacy';
+$lang['faq_menu_privacy_question_terms']             = 'Terms';
+$lang['faq_menu_privacy_answer_terms']               = 'By accessing the website at http://link-base.org,</p>
+<p>you are agreeing to be bound by these terms of service,</p>
+<p>all applicable laws and regulations,</p>
+<p>and agree that you are responsible for compliance with any applicable local laws.</p>
+<p>If you do not agree with any of these terms, you are prohibited from using or accessing this site.</p>
+<p>The materials contained in this website are protected by applicable copyright and trademark law.';
+$lang['faq_menu_privacy_question_useLicense']        = 'Use License';
+$lang['faq_menu_privacy_answer_useLicense']          = 'a) Permission is granted to temporarily download one copy of the materials
+(information or software) on LinkBaseORG\'s website for personal, non-commercial transitory viewing only.
+This is the grant of a license, not a transfer of title, and under this license you may not:</p>
+<p>I: modify or copy the materials;</p>
+<p>II: use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</p>
+<p>III: attempt to decompile or reverse engineer any software contained on LinkBaseORG\'s website;</p>
+<p>IV: remove any copyright or other proprietary notations from the materials; or</p>
+<p>V: transfer the materials to another person or "mirror" the materials on any other server.</p>
+<p>b) This license shall automatically terminate if you violate any of these restrictions and may be terminated by LinkBaseORG at any time.
+Upon terminating your viewing of these materials or upon the termination of this license,
+you must destroy any downloaded materials in your possession whether in electronic or printed format.';
+$lang['faq_menu_privacy_question_disclaimer']        = 'Disclaimer';
+$lang['faq_menu_privacy_answer_disclaimer']          = 'a) The materials on LinkBaseORG\'s website are provided on an \'as is\' basis. LinkBaseORG makes no warranties,
+expressed or implied, and hereby disclaims and negates all other warranties including, without limitation,
+implied warranties or conditions of merchantability, fitness for a particular purpose,
+or non-infringement of intellectual property or other violation of rights.</p>
+<p>b) Further, LinkBaseORG does not warrant or make any representations concerning the accuracy,
+likely results,
+or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.';
+$lang['faq_menu_privacy_question_limitations']       = 'Limitations';
+$lang['faq_menu_privacy_answer_limitations']         = 'In no event shall LinkBaseORG or its suppliers be liable for any damages (including, without limitation,
+damages for loss of data or profit,
+or due to business interruption) arising out of the use or inability to use the materials on LinkBaseORG\'s website,
+even if LinkBaseORG or a LinkBaseORG authorized representative has been notified orally or in writing of the possibility of such damage.
+Because some jurisdictions do not allow limitations on implied warranties,
+or limitations of liability for consequential or incidental damages, these limitations may not apply to you.';
+$lang['faq_menu_privacy_question_accuracy']          = 'Accuracy of materials';
+$lang['faq_menu_privacy_answer_accuracy']            = 'The materials appearing on LinkBaseORG website could include technical, typographical, or photographic errors.
+LinkBaseORG does not warrant that any of the materials on its website are accurate, complete or current.
+LinkBaseORG may make changes to the materials contained on its website at any time without notice.
+However LinkBaseORG does not make any commitment to update the materials.';
+$lang['faq_menu_privacy_question_links']             = 'Links';
+$lang['faq_menu_privacy_answer_links']               = 'LinkBaseORG has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site.
+The inclusion of any link does not imply endorsement by LinkBaseORG of the site.
+Use of any such linked website is at the user\'s own risk.';
+$lang['faq_menu_privacy_question_modifications']     = 'Modifications';
+$lang['faq_menu_privacy_answer_modifications']       = 'LinkBaseORG may revise these terms of service for its website at any time without notice.
+By using this website you are agreeing to be bound by the then current version of these terms of service.';
+$lang['faq_menu_privacy_question_governingLaw']      = 'Governing Law';
+$lang['faq_menu_privacy_answer_governingLaw']        = 'These terms and conditions are governed by and construed in accordance with the laws of Malaysia
+and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.';
+$lang['faq_menu_privacy_question_piwik']             = 'Matomo (previously Piwik)';
+$lang['faq_menu_privacy_answer_piwik']               = 'Disable user tracking via Piwik';
+// Advertisement
+$lang['faq_menu_advertisement']                      = 'Advertisement';
+$lang['faq_menu_advertisement_question_banner']      = 'Advertisement';
+$lang['faq_menu_advertisement_answer_banner']        = 'Big banner -> small banner -> custom banner';
+?>
