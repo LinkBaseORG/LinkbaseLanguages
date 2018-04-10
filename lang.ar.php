46a0866cb2d58d527d57782aba3e8605cafd6983
LinkBaseORG <38041853+LinkBaseORG@users.noreply.github.com>
2018-04-10 14:07:31 +0200

    Arabic

---
+++ lang.ar.php
@@ -0,0 +1,259 @@
+<?php
+// Global
+$lang['site_error_backText']        = 'إلى الوراء';
+$lang['site_403_errorText']         = 'عذرًا ، ليس لديك إذن للوصول إلى هذه الصفحة.';
+$lang['site_404_errorText']         = 'عفوا! عذرا ، هذه الصفحة لا يمكن العثور عليها.';
+$lang['site_500_errorText']         = 'عذرًا ، حدث خطأ داخلي.';
+// Navigation
+$lang['navigation_toggle']          = 'تبديل التنقل';
+$lang['navigation_home']            = 'الصفحة الرئيسية';
+// NavigationSub
+$lang['navigation_boards']          = 'المنتديات';
+$lang['navigation_boards_all']      = 'كل المنتديات';
+$lang['navigation_boards_noFraud']  = 'لا الحيل';
+$lang['navigation_boards_tor']      = 'Tor فقط';
+$lang['navigation_boards_i2p']      = 'I2P فقط';
+// Navigation
+$lang['navigation_blogs']           = 'بلوق';
+$lang['navigation_shops']           = 'محلات';
+$lang['navigation_warez']           = 'ورز';
+$lang['navigation_useful']          = 'مفيد';
+$lang['navigation_vpn']             = 'فبن';
+$lang['navigation_hosting']         = 'استضافة';
+$lang['navigation_addLink']         = 'أضف الرابط';
+$lang['navigation_last5Added']      = 'عرض الروابط التي تم تغييرها مؤخرًا (RSS)';
+// NavigationSub
+$lang['navigation_information']                       = 'معلومات';
+$lang['navigation_information_contact']               = 'اتصال';
+$lang['navigation_information_donate']                = 'تبرع';
+$lang['navigation_information_pricing']               = 'أسعار';
+$lang['navigation_information_linkUs']                = 'اربطنا';
+$lang['navigation_information_misc']                  = 'آخر';
+$lang['navigation_information_misc_changelog']        = 'التغيير';
+$lang['navigation_information_misc_api']              = 'API';
+$lang['navigation_information_misc_faq']              = 'التعليمات';
+$lang['navigation_information_misc_rssAdded']         = '[وأضاف] RSS';
+$lang['navigation_information_misc_rssUpdated']       = '[تغيرت] RSS';
+$lang['navigation_information_misc_siteUptime']       = 'صفحة الحالة';
+// Breadcrumbs
+$lang['navigation_breadcrumb_home']       = 'الصفحة الرئيسية';
+$lang['navigation_breadcrumb_noFraud']    = 'فقط NonFraud المنتديات';
+$lang['navigation_breadcrumb_tor']        = 'فقط TOR المنتديات';
+$lang['navigation_breadcrumb_i2p']        = 'فقط I2P المنتديات';
+$lang['navigation_breadcrumb_search']     = 'نتائج البحث';
+$lang['navigation_breadcrumb_blogs']      = 'الأمن والقرصنة المدونات';
+$lang['navigation_breadcrumb_shops']      = 'محلات';
+$lang['navigation_breadcrumb_warez']      = 'ورز';
+$lang['navigation_breadcrumb_useful']     = 'روابط مفيدة';
+$lang['navigation_breadcrumb_vpn']        = 'مزود VPN';
+$lang['navigation_breadcrumb_hosting']    = 'مزود استضافة';
+$lang['navigation_breadcrumb_addLink']    = 'أضف رابطًا جديدًا';
+$lang['navigation_breadcrumb_reportLink'] = 'تقرير عن وصلة ميتة';
+$lang['navigation_breadcrumb_contact']    = 'اتصل بنا';
+$lang['navigation_breadcrumb_donate']     = 'جمع التبرعات';
+$lang['navigation_breadcrumb_pricing']    = 'خطط أسعار الإعلانات الفضائية';
+$lang['navigation_breadcrumb_linkUs']     = 'اربطنا';
+$lang['navigation_breadcrumb_changelog']  = 'نسخة التغيير';
+$lang['navigation_breadcrumb_faq']        = 'التعليمات';
+// Subnavigation
+$lang['subnavigation_lastDbUpdate']       = 'آخر تحديث لقاعدة البيانات';
+// Sidebar
+$lang['sidebar_searchPlaceholder']    = 'بحث';
+$lang['sidebar_linkQueue']            = 'قائمة انتظار الارتباط';
+$lang['sidebar_linkQueue_added']      = 'وأضاف';
+$lang['sidebar_linkQueue_reported']   = 'ذكرت';
+$lang['sidebar_statistics']           = 'إحصائيات';
+$lang['sidebar_statistics_online']    = 'على الخط';
+$lang['sidebar_statistics_offline']   = 'حاليا';
+$lang['sidebar_statistics_languages'] = 'اللغات';
+$lang['sidebar_statistics_total']     = 'شامل';
+$lang['sidebar_advertisement']        = 'إعلان';
+$lang['sidebar_version']              = 'الإصدار 4.1';
+$lang['sidebar_version_changelog']    = 'التغيير';
+// Link us site
+$lang['linkus_htmlCodeText']          = 'احصل على كود HTML لتضمين شعارنا في موقعك.';
+$lang['linkus_bbcodeText']            = 'احصل على كود HTML لإدراج شعارنا في التواقيع ، على سبيل المثال. في المنتديات (MyBB ، vBulletin ، ...)';
+// Donate site
+$lang['donate_headText']              = 'تبرع للحفاظ على هذه الصفحة على قيد الحياة:';
+// Changelog site
+$lang['changelog_head']         = 'التغيير';
+$lang['changelog_committed']    = 'أصدرت نسخة جديدة';
+// Pricing site
+$lang['pricing_smallHead']      = 'صغير';
+$lang['pricing_smallInclude']   = 'get featured "النجم الاحمر"';
+$lang['pricing_mediumHead']     = 'متوسط';
+$lang['pricing_highlight']      = 'الخطة الأكثر شعبية';
+$lang['pricing_mediumInclude']  = '468px * 60px (+النجم الاحمر)';
+$lang['pricing_largeHead']      = 'عظيم';
+$lang['pricing_largeInclude']   = '920px * 120px (+النجم الاحمر)';
+$lang['pricing_perMonth']       = 'في الشهر';
+$lang['pricing_contactUs']      = 'اتصل بنا';
+// Search Site
+$lang['search_nameText']                = 'أسماء';
+$lang['search_typeHead']                = 'نوع';
+$lang['search_resetText']               = 'إعادة البحث';
+$lang['search_category']                = 'فئة';
+$lang['search_typeBoard']               = 'منتدى';
+$lang['search_typeBlog']                = 'بلوق';
+$lang['search_typeWarez']               = 'ورز';
+$lang['search_typeVPN']                 = 'VPN';
+$lang['search_typeUseful']              = 'مفيد';
+$lang['search_typeShop']                = 'متجر';
+$lang['search_typeHosting']             = 'استضافة';
+// Contact us Site
+$lang['contact_donateText']             = 'تبرع للحفاظ على هذه الصفحة على قيد الحياة:';
+$lang['contact_contactInformationText'] = 'معلومات الاتصال:';
+$lang['contact_twitterFollow']          = 'تابع';
+$lang['contact_warningHeader']          = 'تحذير!';
+$lang['contact_warningText']            = 'إذا كنت ترغب في الاتصال بنا ، فاستخدم فقط معلومات الاتصال الموضحة أعلاه لمقاومة الاحتيال عن طريق المزيف!';
+// Report Link Site
+$lang['report_headText']              = 'الإبلاغ عن وصلة ميتة';
+$lang['report_sitename']              = 'أسماء';
+$lang['report_url']                   = 'نطاق';
+$lang['report_reason']                = 'سبب';
+$lang['report_reportBtn']             = 'رابط التقرير';
+$lang['report_success_title']         = 'نجاح!';
+$lang['report_success_text']          = 'تم الإبلاغ عن الرابط. شكرا لك على مساعدتك';
+$lang['report_fail_title']            = 'تحذير!';
+$lang['report_fail_text']             = 'كان هناك خطأ. يرجى المحاولة مرة أخرى.';
+$lang['report_fail_DoubleEntry_text'] = 'تم الإعلان عن هذا الرابط بالفعل.';
+
+// Add Link Site
+$lang['add_url']                      = 'نطاق';
+$lang['add_tor']                      = 'نطاق TOR';
+$lang['add_i2p']                      = 'نطاق I2P';
+$lang['add_facebook']                 = 'الفيسبوك';
+$lang['add_twitter']                  = 'Twitter';
+$lang['add_category']                 = 'فئة';
+$lang['add_languages']                = 'لغة';
+$lang['add_pleaseSelect']             = 'اختر من فضلك';
+$lang['add_information']              = 'معلومات';
+$lang['add_information_placeholder']  = 'مثلا نطاقات النسخ الاحتياطي ومعلومات الاتصال ... (بحد أقصى 200 حرف)';
+$lang['add_addLink']                  = 'أضف الرابط';
+$lang['add_success_title']            = 'نجاح!';
+$lang['add_success_text']             = 'الرابط قد أضيف. شكرا لك على مساعدتك.';
+$lang['add_fail_title']               = 'خطأ!';
+$lang['add_fail_text']                = 'حدث خطأ ، يرجى المحاولة مرة أخرى.';
+$lang['add_facebook_placeholder']     = 'اسم المجموعة';
+// Hosting Site
+$lang['hosting_head']                 = '';
+$lang['hosting_service']              = 'مزود';
+$lang['hosting_creditcard']           = 'بطاقة إئتمان';
+$lang['hosting_locations']            = 'مواقع';
+$lang['hosting_infoTextHead']         = 'معلومات!';
+$lang['hosting_infoText']             = 'يستكشف نورتون العالم السري لاستضافة مضاد للرصاص ، مخبأ في عمق المخابئ تحت الأرض
+معزولة في البحر وتوزيعها على شبكة الإنترنت. اكتشف التهديدات لهذه الخدمات ، مثل شبكات البرامج الضارة والبرامج الضارة والفدية والسوق السوداء
+وتعلم كيف يمكنك حماية نفسك "The Most Dangerous Town on the Internet - Where Cybercrime Goes to Hide"';
+$lang['hosting_infoStartVideo']       = 'بدء الفيديو';
+// Footer
+$lang['footer_generatedInStart']      = 'Site generated in ';
+$lang['footer_generatedInEnd']        = ' seconds.';
+// Language Category
+$lang['promoted']        = '&#10035;إعلان';
+$lang['partner']         = 'شريك';
+$lang['al']              = 'الألبانية';
+$lang['ar']              = 'العربية';
+$lang['az']              = 'أذربيجان';
+$lang['bs']              = 'البوسنية';
+$lang['zh']              = 'الصينية';
+$lang['hr']              = 'الكرواتية';
+$lang['cz']              = 'تشيكي';
+$lang['da']              = 'دانماركي';
+$lang['nl']              = 'هولندي';
+$lang['en']              = 'الإنجليزية';
+$lang['fr']              = 'اللغة الفرنسية';
+$lang['ge']              = 'الجورجية';
+$lang['de']              = 'ألماني';
+$lang['gr']              = 'اللغة اليونانية';
+$lang['id']              = 'الأندونيسية';
+$lang['it']              = 'الإيطالي';
+$lang['jp']              = 'اليابانية';
+$lang['my']              = 'الماليزية';
+$lang['fas']             = 'اللغة الفارسية';
+$lang['pl']              = 'البولندية';
+$lang['pt']              = 'البرتغالية';
+$lang['ro']              = 'الرومانية';
+$lang['ru']              = 'الروسية';
+$lang['sr']              = 'صربي';
+$lang['es']              = 'الأسبانية';
+$lang['th']              = 'التايلاندية';
+$lang['tr']              = 'اللغة التركية';
+$lang['ua']              = 'الأوكراني';
+$lang['uz']              = 'الأوزبكي';
+$lang['vn']              = 'الفيتنامية';
+// Shop Category
+$lang['shops_ccv']              = 'السيرة الذاتية / مقالب';
+$lang['shops_accounts']         = 'حسابات';
+$lang['shops_icq']              = 'ICQ';
+$lang['shops_drugs']            = 'المخدرات';
+$lang['shops_rdp']              = 'Rdp / Smtp';
+$lang['shops_flood']            = 'فيضان';
+$lang['shops_exploit']          = 'استغلال';
+$lang['shops_cccheck']          = 'فاحص CC';
+$lang['shops_iptv']             = 'IPTV';
+$lang['shops_other']            = 'آخر';
+// Useful Category
+$lang['useful_vuln']            = 'نقاط الضعف';
+$lang['useful_unsorted']        = 'لم يتم فرزها';
+$lang['useful_ffaddons']        = 'إضافات Firefox';
+$lang['useful_cryptography']    = 'التشفير';
+$lang['useful_scanner']         = 'الماسح الضوئي';
+$lang['useful_antimalware']     = 'مكافحة البرامج الضارة';
+$lang['useful_linklist']        = 'قوائم صلة';
+$lang['useful_multiuploader']   = 'Multiuploader';
+$lang['useful_exchanger']       = 'مبادل';
+$lang['useful_trashmail']       = 'Trash Mail';
+$lang['useful_challenges']      = 'التحديات';
+$lang['useful_securemail']      = 'Secure Mail';
+$lang['useful_cardsharing']     = 'Cardsharing';
+$lang['useful_toplist']         = 'قائمة أعلى';
+$lang['useful_nfo']             = 'NFO';
+$lang['useful_ddosprotect']     = 'حماية ddos';
+$lang['useful_cryptocurrency']  = 'العملات التشفير';
+$lang['useful_documentation']   = 'توثيق';
+$lang['useful_linkcrypter']     = 'Linkcrypter';
+$lang['useful_startpage']       = 'الصفحات الرئيسية';
+$lang['useful_telegramgrp']     = 'مجموعة Telegram';
+$lang['useful_uptimechecker']   = 'مدقق وقت التشغيل';
+$lang['useful_pastebin']        = 'Pastebin';
+$lang['useful_security']        = 'أمن';
+$lang['useful_hackgame']        = 'العاب هاك';
+$lang['useful_serials']         = 'الأرقام التسلسلية';
+// VPN Category
+$lang['vpn_paidvpn']            = 'مدفوع VPN';
+$lang['vpn_vpninfo']            = 'معلومات VPN';
+$lang['vpn_freevpn']            = 'حر VPN';
+$lang['vpn_socks']              = 'Socks';
+$lang['vpn_webproxy']           = 'Webproxy';
+$lang['vpn_checker']            = 'Checker';
+// Warez Category
+$lang['warez_boards']           = 'المنتديات';
+$lang['warez_anime']            = 'أنيمي / هنتاي / مانغا';
+$lang['warez_tvstream']         = 'تيار التلفزيون';
+$lang['warez_ftp']              = 'FTP / FXP';
+$lang['warez_documentation']    = 'توثيق';
+$lang['warez_ebook']            = 'يبوك';
+$lang['warez_xdcc']             = 'XDCC';
+$lang['warez_music']            = 'موسيقى';
+$lang['warez_games']            = 'ألعاب';
+$lang['warez_movies']           = 'أفلام';
+$lang['warez_mixed']            = 'اهتزت';
+$lang['warez_android']          = 'Android';
+$lang['warez_apple']            = 'Apple';
+$lang['warez_moviestreams']     = 'دفق الفيلم';
+$lang['warez_search']           = 'بحث';
+$lang['warez_porn']             = 'إباحية';
+$lang['warez_usenet']           = 'NZB / Usenet';
+$lang['warez_scripts']          = 'Scripts';
+$lang['warez_torrent']          = 'Torrent';
+// Board Redirection Site
+$lang['out_home']               = 'الصفحة الرئيسية';
+$lang['out_report']             = 'الإبلاغ عن وصلة ميتة';
+$lang['out_voteForUs']          = 'صوّت لنا';
+$lang['out_vote']               = 'صوت';
+$lang['out_closeFrame']         = 'أغلق النافذة';
+$lang['out_redirectingTo']      = 'إعادة توجيه إلى ';
+$lang['out_redirectingIn']      = ' في 20 ثانية.';
+$lang['out_iframeText1']        = 'لا يمكن لمتصفحك عرض إطارات iframe:';
+$lang['out_iframeText2']        = 'انقر هنا لعرض الصفحة المطلوبة:';
+?>
