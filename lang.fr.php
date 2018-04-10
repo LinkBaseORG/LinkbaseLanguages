11c98d356736dd09464d2be9ebbf93c5ac991ee4
LinkBaseORG <38041853+LinkBaseORG@users.noreply.github.com>
2018-04-10 16:08:27 +0200

    French

---
+++ lang.fr.php
@@ -0,0 +1,259 @@
+<?php
+// Global
+$lang['site_error_backText']        = 'Dos';
+$lang['site_403_errorText']         = 'Désolé, vous n\'êtes pas autorisé à accéder à cette page.';
+$lang['site_404_errorText']         = 'Oops! Désolé, cette page n\'a pu être trouvée.';
+$lang['site_500_errorText']         = 'Désolé, il y a eu une erreur interne.';
+// Navigation
+$lang['navigation_toggle']          = 'Changer de navigation';
+$lang['navigation_home']            = 'Maison';
+// NavigationSub
+$lang['navigation_boards']          = 'Forums';
+$lang['navigation_boards_all']      = 'Tous les forums';
+$lang['navigation_boards_noFraud']  = 'Pas de fraude';
+$lang['navigation_boards_tor']      = 'Seulement Tor';
+$lang['navigation_boards_i2p']      = 'Seulement I2p';
+// Navigation
+$lang['navigation_blogs']           = 'Blogs';
+$lang['navigation_shops']           = 'Magasins';
+$lang['navigation_warez']           = 'Warez';
+$lang['navigation_useful']          = 'Utile';
+$lang['navigation_vpn']             = 'VPN';
+$lang['navigation_hosting']         = 'Hébergement';
+$lang['navigation_addLink']         = 'Ajouter un lien';
+$lang['navigation_last5Added']      = 'Afficher les liens récemment modifiés (RSS)';
+// NavigationSub
+$lang['navigation_information']                       = 'Information';
+$lang['navigation_information_contact']               = 'Contact';
+$lang['navigation_information_donate']                = 'Donner';
+$lang['navigation_information_pricing']               = 'Prix';
+$lang['navigation_information_linkUs']                = 'Nous relier';
+$lang['navigation_information_misc']                  = 'Autres';
+$lang['navigation_information_misc_changelog']        = 'Changelog';
+$lang['navigation_information_misc_api']              = 'API';
+$lang['navigation_information_misc_faq']              = 'FAQ';
+$lang['navigation_information_misc_rssAdded']         = 'RSS [ajoutée]';
+$lang['navigation_information_misc_rssUpdated']       = 'RSS [modifié]';
+$lang['navigation_information_misc_siteUptime']       = 'Page d\'état';
+// Breadcrumbs
+$lang['navigation_breadcrumb_home']       = 'Maison';
+$lang['navigation_breadcrumb_noFraud']    = 'Pas de fraude';
+$lang['navigation_breadcrumb_tor']        = 'Seulement TOR forums';
+$lang['navigation_breadcrumb_i2p']        = 'Seulement I2P forums';
+$lang['navigation_breadcrumb_search']     = 'Résultats de la recherche';
+$lang['navigation_breadcrumb_blogs']      = 'Sécurité et piratage de blogs';
+$lang['navigation_breadcrumb_shops']      = 'Magasins';
+$lang['navigation_breadcrumb_warez']      = 'Warez';
+$lang['navigation_breadcrumb_useful']     = 'Liens utiles';
+$lang['navigation_breadcrumb_vpn']        = 'VPN fournisseur';
+$lang['navigation_breadcrumb_hosting']    = 'Hosting fournisseur';
+$lang['navigation_breadcrumb_addLink']    = 'Ajouter un nouveau lien';
+$lang['navigation_breadcrumb_reportLink'] = 'Rapport d\'un lien mort';
+$lang['navigation_breadcrumb_contact']    = 'Contactez-nous';
+$lang['navigation_breadcrumb_donate']     = 'Donner';
+$lang['navigation_breadcrumb_pricing']    = 'Plans de prix de l\'espace publicitaire';
+$lang['navigation_breadcrumb_linkUs']     = 'Nous relier';
+$lang['navigation_breadcrumb_changelog']  = 'Version changelog';
+$lang['navigation_breadcrumb_faq']        = 'FAQ';
+// Subnavigation
+$lang['subnavigation_lastDbUpdate']       = 'Dernière mise à jour';
+// Sidebar
+$lang['sidebar_searchPlaceholder']    = 'Recherche';
+$lang['sidebar_linkQueue']            = 'File d\'attente';
+$lang['sidebar_linkQueue_added']      = 'ajoutée';
+$lang['sidebar_linkQueue_reported']   = 'rapporté';
+$lang['sidebar_statistics']           = 'Statistiques';
+$lang['sidebar_statistics_online']    = 'en ligne';
+$lang['sidebar_statistics_offline']   = 'hors ligne';
+$lang['sidebar_statistics_languages'] = 'Langues';
+$lang['sidebar_statistics_total']     = 'global';
+$lang['sidebar_advertisement']        = 'Publicité';
+$lang['sidebar_version']              = 'Version 4.1';
+$lang['sidebar_version_changelog']    = 'Changelog';
+// Link us site
+$lang['linkus_htmlCodeText']          = 'Obtenez le code HTML pour intégrer notre bannière dans votre site Web.';
+$lang['linkus_bbcodeText']            = 'Obtenez le code HTML pour insérer notre bannière dans les signatures, par ex. dans les forums (MyBB, vBulletin, ...)';
+// Donate site
+$lang['donate_headText']              = 'Faire un don pour garder cette page en vie:';
+// Changelog site
+$lang['changelog_head']         = 'Changelog';
+$lang['changelog_committed']    = 'hat eine neue Version veröffentlicht';
+// Pricing site
+$lang['pricing_smallHead']      = 'Petit';
+$lang['pricing_smallInclude']   = '"étoile rouge"';
+$lang['pricing_mediumHead']     = 'Moyens';
+$lang['pricing_highlight']      = 'Plan le plus populaire';
+$lang['pricing_mediumInclude']  = '468px * 60px (+étoile rouge)';
+$lang['pricing_largeHead']      = 'Grand';
+$lang['pricing_largeInclude']   = '920px * 120px (+étoile rouge)';
+$lang['pricing_perMonth']       = 'par mois';
+$lang['pricing_contactUs']      = 'Contactez-nous';
+// Search Site
+$lang['search_nameText']                = 'Nom';
+$lang['search_typeHead']                = 'Type';
+$lang['search_resetText']               = 'Réinitialiser la recherche';
+$lang['search_category']                = 'Catégorie';
+$lang['search_typeBoard']               = 'Forum';
+$lang['search_typeBlog']                = 'Blog';
+$lang['search_typeWarez']               = 'Warez';
+$lang['search_typeVPN']                 = 'VPN';
+$lang['search_typeUseful']              = 'Utile';
+$lang['search_typeShop']                = 'Magasins';
+$lang['search_typeHosting']             = 'Hébergement';
+// Contact us Site
+$lang['contact_donateText']             = 'Faire un don pour garder cette page en vie:';
+$lang['contact_contactInformationText'] = 'Coordonnées:';
+$lang['contact_twitterFollow']          = 'Suivre';
+$lang['contact_warningHeader']          = 'Avertissement!';
+$lang['contact_warningText']            = 'Wenn du uns kontaktieren möchtest, nutze <b>ausschließlich</b> die <b>oben</b> angezeigten Kontaktinformationen, um einen Betrug durch Fake\'s entgegenzuwirken!';
+// Report Link Site
+$lang['report_headText']              = 'Signaler un lien mort';
+$lang['report_sitename']              = 'Nom';
+$lang['report_url']                   = 'Domaine';
+$lang['report_reason']                = 'Raison';
+$lang['report_reportBtn']             = 'Lien de rapport';
+$lang['report_success_title']         = 'Succès!';
+$lang['report_success_text']          = 'Le lien a été signalé. Merci pour votre aide.';
+$lang['report_fail_title']            = 'Avertissement!';
+$lang['report_fail_text']             = 'Il y avait une erreur. Veuillez réessayer.';
+$lang['report_fail_DoubleEntry_text'] = 'Ce lien a déjà été rapporté.';
+
+// Add Link Site
+$lang['add_url']                      = 'Domaine';
+$lang['add_tor']                      = 'TOR Domaine';
+$lang['add_i2p']                      = 'I2P Domaine';
+$lang['add_facebook']                 = 'Facebook';
+$lang['add_twitter']                  = 'Twitter';
+$lang['add_category']                 = 'Catégorie';
+$lang['add_languages']                = 'Langue';
+$lang['add_pleaseSelect']             = 'choisissez s\'il vous plaît';
+$lang['add_information']              = 'Information';
+$lang['add_information_placeholder']  = 'par exemple, Domaines de sauvegarde, informations de contact, ... (200 caractères maximum)';
+$lang['add_addLink']                  = 'Ajouter un lien';
+$lang['add_success_title']            = 'Succès!';
+$lang['add_success_text']             = 'Le lien a été ajouté. Merci pour votre aide.';
+$lang['add_fail_title']               = 'Erreur!';
+$lang['add_fail_text']                = 'Une erreur est survenue, veuillez réessayer.';
+$lang['add_facebook_placeholder']     = 'Nom du groupe';
+// Hosting Site
+$lang['hosting_head']                 = '';
+$lang['hosting_service']              = 'Fournisseur';
+$lang['hosting_creditcard']           = 'Carte de crédit';
+$lang['hosting_locations']            = 'Endroits';
+$lang['hosting_infoTextHead']         = 'Information!';
+$lang['hosting_infoText']             = 'Norton explore le monde secret de l\'hébergement pare-balles, caché dans des bunkers souterrains
+isolé en mer et distribué sur le web. Découvrez les menaces qui pèsent sur ces services, tels que les réseaux de robots, les logiciels malveillants, les rançongiciels et le marché noir
+et apprenez comment vous pouvez vous protéger dans "The Most Dangerous Town on the Internet - Where Cybercrime Goes to Hide"';
+$lang['hosting_infoStartVideo']       = 'Démarrer la vidéo';
+// Footer
+$lang['footer_generatedInStart']      = 'La page a été générée en ';
+$lang['footer_generatedInEnd']        = ' secondes.';
+// Language Category
+$lang['promoted']        = '&#10035;Publicité';
+$lang['partner']         = 'Partenaire';
+$lang['al']              = 'Albanais';
+$lang['ar']              = 'Arabe';
+$lang['az']              = 'Azerbaïdjanais';
+$lang['bs']              = 'Bosnie';
+$lang['zh']              = 'Chinois';
+$lang['hr']              = 'Croate';
+$lang['cz']              = 'Tchèque';
+$lang['da']              = 'Danois';
+$lang['nl']              = 'Néerlandais';
+$lang['en']              = 'Anglais';
+$lang['fr']              = 'Français';
+$lang['ge']              = 'Géorgien';
+$lang['de']              = 'Allemand';
+$lang['gr']              = 'Grecque';
+$lang['id']              = 'Indonésien';
+$lang['it']              = 'Italian';
+$lang['jp']              = 'Japonais';
+$lang['my']              = 'Malaisie';
+$lang['fas']             = 'Persan';
+$lang['pl']              = 'Polonais';
+$lang['pt']              = 'Portugais';
+$lang['ro']              = 'Roumain';
+$lang['ru']              = 'Russe';
+$lang['rs']              = 'Serbe';
+$lang['es']              = 'Espagnol';
+$lang['th']              = 'Thaï';
+$lang['tr']              = 'Turc';
+$lang['ua']              = 'Ukrainien';
+$lang['uz']              = 'Ouzbek';
+$lang['vn']              = 'Vietnamien';
+// Shop Category
+$lang['shops_ccv']              = 'Ccv / Dumps';
+$lang['shops_accounts']         = 'Comptes';
+$lang['shops_icq']              = 'ICQ';
+$lang['shops_drugs']            = 'Médicaments';
+$lang['shops_rdp']              = 'Rdp / Smtp';
+$lang['shops_flood']            = 'Inondation';
+$lang['shops_exploit']          = 'Exploit';
+$lang['shops_cccheck']          = 'CC Checker';
+$lang['shops_iptv']             = 'IPTV';
+$lang['shops_other']            = 'Autre';
+// Useful Category
+$lang['useful_vuln']            = 'Vulnérabilités';
+$lang['useful_unsorted']        = 'Non triés';
+$lang['useful_ffaddons']        = 'Firefox addons';
+$lang['useful_cryptography']    = 'Cryptographie';
+$lang['useful_scanner']         = 'Scanner';
+$lang['useful_antimalware']     = 'Anti Malware';
+$lang['useful_linklist']        = 'Listes de liens';
+$lang['useful_multiuploader']   = 'Multiuploader';
+$lang['useful_exchanger']       = 'Echangeur';
+$lang['useful_trashmail']       = 'Trash courrier';
+$lang['useful_challenges']      = 'Défis';
+$lang['useful_securemail']      = 'Sécurisé courrier';
+$lang['useful_cardsharing']     = 'Cardsharing';
+$lang['useful_toplist']         = 'Toplists';
+$lang['useful_nfo']             = 'NFO';
+$lang['useful_ddosprotect']     = 'Protection DDoS';
+$lang['useful_cryptocurrency']  = 'Devises crypto';
+$lang['useful_documentation']   = 'Documentation';
+$lang['useful_linkcrypter']     = 'Linkcrypter';
+$lang['useful_startpage']       = 'Les pages d\'accueil';
+$lang['useful_telegramgrp']     = 'Telegram groupes';
+$lang['useful_uptimechecker']   = 'Vérificateur de disponibilité';
+$lang['useful_pastebin']        = 'Pastebin';
+$lang['useful_security']        = 'Sécurité';
+$lang['useful_hackgame']        = 'Jeux Hack';
+$lang['useful_serials']         = 'Numéros de série';
+// VPN Category
+$lang['vpn_paidvpn']            = 'Payé VPN';
+$lang['vpn_vpninfo']            = 'VPN information';
+$lang['vpn_freevpn']            = 'Gratuit VPN';
+$lang['vpn_socks']              = 'Socks';
+$lang['vpn_webproxy']           = 'Webproxy';
+$lang['vpn_checker']            = 'Checker';
+// Warez Category
+$lang['warez_boards']           = 'Forums';
+$lang['warez_anime']            = 'Anime / Hentai / Manga';
+$lang['warez_tvstream']         = 'Flux TV';
+$lang['warez_ftp']              = 'FTP / FXP';
+$lang['warez_documentation']    = 'Documentation';
+$lang['warez_ebook']            = 'Ebook';
+$lang['warez_xdcc']             = 'XDCC';
+$lang['warez_music']            = 'Musique';
+$lang['warez_games']            = 'Jeux';
+$lang['warez_movies']           = 'Films';
+$lang['warez_mixed']            = 'Secoué';
+$lang['warez_android']          = 'Android';
+$lang['warez_apple']            = 'Apple';
+$lang['warez_moviestreams']     = 'Flux de film';
+$lang['warez_search']           = 'Recherche';
+$lang['warez_porn']             = 'Porno';
+$lang['warez_usenet']           = 'NZB / Usenet';
+$lang['warez_scripts']          = 'Scripts';
+$lang['warez_torrent']          = 'Torrent';
+// Board Redirection Site
+$lang['out_home']               = 'Maison';
+$lang['out_report']             = 'Signaler un lien mort';
+$lang['out_voteForUs']          = 'Votez pour nous';
+$lang['out_vote']               = 'Vote';
+$lang['out_closeFrame']         = 'Fermer iframe';
+$lang['out_redirectingTo']      = 'Envoi à ';
+$lang['out_redirectingIn']      = ' en 2 secondes.';
+$lang['out_iframeText1']        = 'Votre navigateur ne peut pas afficher les iframes:';
+$lang['out_iframeText2']        = 'Cliquez ici pour afficher la page demandée:';
+?>
