6203da094e36ab824d3b73028dccdff0d18f2835
LinkBaseORG <38041853+LinkBaseORG@users.noreply.github.com>
2018-04-10 14:07:16 +0200

    Japanese

---
+++ lang.jp.php
@@ -0,0 +1,260 @@
+<?php
+// Global
+$lang['site_error_backText']        = 'バック';
+$lang['site_403_errorText']         = '申し訳ありませんが、このページにアクセスする権限がありません。';
+$lang['site_404_errorText']         = 'おっと！ 申し訳ありませんが、このページは見つかりませんでした。';
+$lang['site_500_errorText']         = '申し訳ありませんが、内部的な間違いがありました。';
+// Navigation
+$lang['navigation_toggle']          = 'スイッチナビゲーション';
+$lang['navigation_home']            = '家';
+// NavigationSub
+$lang['navigation_boards']          = 'フォーラム';
+$lang['navigation_boards_all']      = 'すべてのフォーラム';
+$lang['navigation_boards_noFraud']  = 'Non Fraud';
+$lang['navigation_boards_tor']      = 'のみ Tor';
+$lang['navigation_boards_i2p']      = 'のみ I2p';
+// Navigation
+$lang['navigation_blogs']           = 'ウェブログ';
+$lang['navigation_shops']           = 'ショップ';
+$lang['navigation_warez']           = 'ワレズ';
+$lang['navigation_useful']          = '便利';
+$lang['navigation_vpn']             = 'VPN';
+$lang['navigation_hosting']         = 'ホスティング';
+$lang['navigation_addLink']         = 'リンクを追加';
+$lang['navigation_last5Added']      = '最近変更されたリンクを表示する（RSS';
+// NavigationSub
+$lang['navigation_information']                       = '情報';
+$lang['navigation_information_contact']               = '接触';
+$lang['navigation_information_donate']                = '寄付します';
+$lang['navigation_information_pricing']               = '物価';
+$lang['navigation_information_linkUs']                = '私たちにリンク';
+$lang['navigation_information_misc']                  = '他の';
+$lang['navigation_information_misc_changelog']        = '変更履歴';
+$lang['navigation_information_misc_api']              = 'API';
+$lang['navigation_information_misc_faq']              = 'よくある質問';
+$lang['navigation_information_misc_rssAdded']         = 'RSS [追加]';
+$lang['navigation_information_misc_rssUpdated']       = 'RSS [変更]';
+$lang['navigation_information_misc_siteUptime']       = 'ステータスページ';
+// Breadcrumbs
+$lang['navigation_breadcrumb_home']       = '家';
+$lang['navigation_breadcrumb_noFraud']    = 'のみ NonFraud フォーラム';
+$lang['navigation_breadcrumb_tor']        = 'TORフォーラムのみ';
+$lang['navigation_breadcrumb_i2p']        = 'I2Pフォーラムのみ';
+$lang['navigation_breadcrumb_search']     = '検索結果';
+$lang['navigation_breadcrumb_blogs']      = 'セキュリティとハッキングのブログ';
+$lang['navigation_breadcrumb_shops']      = 'ショップ';
+$lang['navigation_breadcrumb_warez']      = 'ワレズ';
+$lang['navigation_breadcrumb_useful']     = '役に立つリンク';
+$lang['navigation_breadcrumb_vpn']        = 'VPN プロバイダ';
+$lang['navigation_breadcrumb_hosting']    = 'ホスティングプロバイダ';
+$lang['navigation_breadcrumb_addLink']    = '新しいリンクを追加';
+$lang['navigation_breadcrumb_reportLink'] = 'デッドリンクの報告';
+$lang['navigation_breadcrumb_contact']    = 'お問い合わせ';
+$lang['navigation_breadcrumb_donate']     = '寄付します';
+$lang['navigation_breadcrumb_pricing']    = '広告スペース価格プラン';
+$lang['navigation_breadcrumb_linkUs']     = '私たちにリンク';
+$lang['navigation_breadcrumb_changelog']  = 'バージョン変更履歴';
+$lang['navigation_breadcrumb_faq']        = 'よくある質問';
+// Subnavigation
+$lang['subnavigation_lastDbUpdate']       = '最後のデータベースの更新';
+// Sidebar
+$lang['sidebar_searchPlaceholder']    = '検索';
+$lang['sidebar_linkQueue']            = 'リンクキュー';
+$lang['sidebar_linkQueue_added']      = '追加';
+$lang['sidebar_linkQueue_reported']   = '報告';
+$lang['sidebar_statistics']           = '統計';
+$lang['sidebar_statistics_online']    = '・オンライン';
+$lang['sidebar_statistics_offline']   = 'オフライン';
+$lang['sidebar_statistics_languages'] = '言語';
+$lang['sidebar_statistics_total']     = '全体的な';
+$lang['sidebar_advertisement']        = '広告';
+$lang['sidebar_version']              = 'バージョン 4.1';
+$lang['sidebar_version_changelog']    = '変更履歴';
+// Link us site
+$lang['linkus_htmlCodeText']          = 'あなたのウェブサイトにバナーを埋め込むためのHTMLコードを入手してください';
+$lang['linkus_bbcodeText']            = 'シグネチャにバナーを挿入するためのHTMLコードを取得します。 フォーラム（MyBB、vBulletin、...）で';
+// Donate site
+$lang['donate_headText']              = 'このページを生き残らせるために寄付する：';
+// Changelog site
+$lang['changelog_head']         = '変更履歴';
+$lang['changelog_committed']    = '新しいバージョンをリリースしました';
+// Pricing site
+$lang['pricing_smallHead']      = '小さな';
+$lang['pricing_smallInclude']   = 'get featured "赤い星"';
+$lang['pricing_mediumHead']     = '手段';
+$lang['pricing_highlight']      = '最も人気のあるプラン';
+$lang['pricing_mediumInclude']  = '468px * 60px (+赤い星)';
+$lang['pricing_largeHead']      = 'すばらしいです';
+$lang['pricing_largeInclude']   = '920px * 120px (+赤い星)';
+$lang['pricing_perMonth']       = '毎月';
+$lang['pricing_contactUs']      = 'お問い合わせ';
+// Search Site
+$lang['search_nameText']                = '名';
+$lang['search_typeHead']                = 'タイプ';
+$lang['search_resetText']               = '検索をリセットする';
+$lang['search_category']                = 'カテゴリ';
+$lang['search_typeBoard']               = 'フォーラム';
+$lang['search_typeBlog']                = 'ウェブログ';
+$lang['search_typeWarez']               = 'ワレズ';
+$lang['search_typeVPN']                 = 'VPN';
+$lang['search_typeUseful']              = '有用';
+$lang['search_typeShop']                = 'ショップ';
+$lang['search_typeHosting']             = 'ホスティング';
+// Contact us Site
+$lang['contact_donateText']             = 'このページを生き残らせるために寄付する：';
+$lang['contact_contactInformationText'] = 'お問い合わせ先：';
+$lang['contact_twitterFollow']          = '結果';
+$lang['contact_warningHeader']          = '警告!';
+$lang['contact_warningText']            = 'にご連絡する場合は、偽の詐欺に対抗するために表示される<b>トップ</b>の連絡先情報を<b>のみ使用</b>してください。';
+// Report Link Site
+$lang['report_headText']              = '死んだリンクを報告する';
+$lang['report_sitename']              = '名';
+$lang['report_url']                   = 'ドメイン';
+$lang['report_reason']                = '理由';
+$lang['report_reportBtn']             = 'レポートリンク';
+$lang['report_success_title']         = '成功!';
+$lang['report_success_text']          = 'リンクが報告されました。 あなたの助けをありがとう.';
+$lang['report_fail_title']            = '警告!';
+$lang['report_fail_text']             = '間違いがありました。 もう一度お試しください.';
+$lang['report_fail_DoubleEntry_text'] = 'このリンクはすでに発表されています.';
+
+// Add Link Site
+$lang['add_url']                      = 'ドメイン';
+$lang['add_tor']                      = 'TOR ドメイン';
+$lang['add_i2p']                      = 'I2P ドメイン';
+$lang['add_facebook']                 = 'Facebook';
+$lang['add_twitter']                  = 'Twitter';
+$lang['add_category']                 = 'カテゴリ';
+$lang['add_languages']                = '言語';
+$lang['add_pleaseSelect']             = '選択してください';
+$lang['add_information']              = '情報';
+$lang['add_information_placeholder']  = '例えば バックアップドメイン、連絡先情報、...（最大200文字';
+$lang['add_addLink']                  = 'リンクを追加';
+$lang['add_success_title']            = '成功!';
+$lang['add_success_text']             = 'リンクが追加されました。 あなたの助けをありがとう.';
+$lang['add_fail_title']               = '警告!';
+$lang['add_fail_text']                = 'エラーが発生しました。もう一度お試しください.';
+$lang['add_facebook_placeholder']     = 'グループ名';
+// Hosting Site
+$lang['hosting_head']                 = '';
+$lang['hosting_service']              = 'プロバイダ';
+$lang['hosting_creditcard']           = 'クレジットカード';
+$lang['hosting_locations']            = '場所';
+$lang['hosting_infoTextHead']         = '情報!';
+$lang['hosting_infoText']             = 'Nortonは地下バンカーに深く隠された防弾ホスティングの秘密の世界を模索しています
+海上で隔離され、ウェブ上に分散される。 ボットネットワーク、マルウェア、ランサムウェア、闇市場など、これらのサービスに対する脅威を発見する
+あなた自身を守る方法を学ぶ "The Most Dangerous Town on the Internet - Where Cybercrime Goes to Hide"';
+$lang['hosting_infoStartVideo']       = 'ビデオを開始する';
+// Footer
+$lang['footer_generatedInStart']      = 'Page generated in ';
+$lang['footer_generatedInEnd']        = ' seconds.';
+// Language Category
+$lang['promoted']        = '&#10035;広告';
+$lang['partner']         = 'パートナー';
+$lang['al']              = 'アルバニア語';
+$lang['ar']              = 'アラビア語';
+$lang['az']              = 'アゼルバイジャン';
+$lang['bs']              = 'ボスニア語';
+$lang['zh']              = '中国の';
+$lang['hr']              = 'クロアチア';
+$lang['cz']              = 'チェコ';
+$lang['da']              = 'デンマーク';
+$lang['nl']              = 'オランダ語';
+$lang['en']              = '英語';
+$lang['fr']              = 'フランス語';
+$lang['ge']              = 'グルジア';
+$lang['de']              = 'ドイツ語';
+$lang['gr']              = 'ギリシャ語';
+$lang['id']              = 'インドネシア';
+$lang['it']              = 'イタリア語';
+$lang['jp']              = '日本の';
+$lang['my']              = 'マレーシア';
+$lang['fas']             = 'ペルシアの';
+$lang['pl']              = 'ポーランド語';
+$lang['pt']              = 'ポルトガル語';
+$lang['ro']              = 'ルーマニア語';
+$lang['ru']              = 'ロシア';
+$lang['sr']              = 'セルビア';
+$lang['es']              = 'スペイン語';
+$lang['th']              = 'タイ';
+$lang['tr']              = 'トルコ語';
+$lang['ua']              = 'ウクライナ';
+$lang['uz']              = 'ウズベク';
+$lang['vn']              = 'ベトナム語';
+// Shop Category
+$lang['shops_ccv']              = 'Ccv / ダンプ';
+$lang['shops_accounts']         = 'アカウント';
+$lang['shops_icq']              = 'ICQ';
+$lang['shops_drugs']            = '麻薬';
+$lang['shops_rdp']              = 'Rdp / Smtp';
+$lang['shops_flood']            = '洪水';
+$lang['shops_exploit']          = '開発します';
+$lang['shops_cccheck']          = 'CC チェッカー';
+$lang['shops_iptv']             = 'IPTV';
+$lang['shops_other']            = '他の';
+// Useful Category
+$lang['useful_vuln']            = '脆弱性';
+$lang['useful_unsorted']        = 'ソートされていません';
+$lang['useful_ffaddons']        = 'Firefox アドオン';
+$lang['useful_cryptography']    = '暗号手法';
+$lang['useful_scanner']         = 'スキャナ';
+$lang['useful_antimalware']     = 'アンチマルウェア';
+$lang['useful_linklist']        = 'リンクリスト';
+$lang['useful_multiuploader']   = 'Multiuploader';
+$lang['useful_exchanger']       = '交換';
+$lang['useful_trashmail']       = 'Trash Mail';
+$lang['useful_challenges']      = '課題';
+$lang['useful_securemail']      = 'Secure Mail';
+$lang['useful_cardsharing']     = 'Cardsharing';
+$lang['useful_toplist']         = 'トップリスト';
+$lang['useful_nfo']             = 'NFO';
+$lang['useful_ddosprotect']     = 'DDoS保護';
+$lang['useful_cryptocurrency']  = '暗号通貨';
+$lang['useful_documentation']   = 'ドキュメンテーション';
+$lang['useful_linkcrypter']     = 'Linkcrypter';
+$lang['useful_startpage']       = 'ホームページ';
+$lang['useful_telegramgrp']     = 'Telegram グループ';
+$lang['useful_uptimechecker']   = '稼働時間チェッカー';
+$lang['useful_pastebin']        = 'Pastebin';
+$lang['useful_security']        = 'セキュリティ';
+$lang['useful_hackgame']        = 'ハックゲーム';
+$lang['useful_serials']         = 'シリアル番号';
+// VPN Category
+$lang['vpn_paidvpn']            = '有料 VPN';
+$lang['vpn_vpninfo']            = 'VPN インフォ';
+$lang['vpn_freevpn']            = '自由な VPN';
+$lang['vpn_socks']              = 'Socks';
+$lang['vpn_webproxy']           = 'Webproxy';
+$lang['vpn_checker']            = 'Checker';
+// Warez Category
+$lang['warez_boards']           = 'フォーラム';
+$lang['warez_anime']            = 'アニメ/ヘンタイ/マンガ';
+$lang['warez_tvstream']         = 'テレビストリーム';
+$lang['warez_ftp']              = 'FTP / FXP';
+$lang['warez_documentation']    = 'ドキュメンテーション';
+$lang['warez_ebook']            = '電子ブック';
+$lang['warez_xdcc']             = 'XDCC';
+$lang['warez_music']            = '音楽';
+$lang['warez_games']            = 'ゲーム';
+$lang['warez_movies']           = 'ムービー';
+$lang['warez_mixed']            = '振とう';
+$lang['warez_android']          = 'アンドロイド';
+$lang['warez_apple']            = 'Apple';
+$lang['warez_moviestreams']     = '映画ストリーム';
+$lang['warez_search']           = '検索';
+$lang['warez_porn']             = 'ポルノ';
+$lang['warez_usenet']           = 'NZB / ユーズネット';
+$lang['warez_scripts']          = 'スクリプト';
+$lang['warez_torrent']          = 'トレント';
+// Board Redirection Site
+$lang['out_home']               = '家';
+$lang['out_report']             = '死んだリンクを報告する';
+$lang['out_voteForUs']          = '私たちに投票してください';
+$lang['out_vote']               = '声';
+$lang['out_closeFrame']         = 'Frame 近いです';
+$lang['out_redirectingTo']      = 'Redirecting to ';
+$lang['out_redirectingIn']      = ' in 20 seconds.';
+$lang['out_iframeText1']        = 'あなたのブラウザはiframeを表示できません:';
+$lang['out_iframeText2']        = 'リクエストされたページを表示するにはここをクリック:';
+// FAQ site
+?>
