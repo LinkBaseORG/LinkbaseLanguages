e6bf92daecf62d35b34b16d2607851f3571f4021
LinkBaseORG <38041853+LinkBaseORG@users.noreply.github.com>
2019-07-10 00:45:53 +0200

    Update lang.zh.php

--- lang.zh.php
+++ lang.zh.php
@@ -11,8 +11,7 @@ $lang['navigation_home']            = '家';
 $lang['navigation_boards']          = '論壇';
 $lang['navigation_boards_all']      = '所有論壇';
 $lang['navigation_boards_noFraud']  = '沒有欺詐';
-$lang['navigation_boards_tor']      = '只 Tor';
-$lang['navigation_boards_i2p']      = '只 I2p';
+$lang['navigation_boards_torI2P']   = '只 Tor / I2P';
 // Navigation
 $lang['navigation_blogs']           = '博客';
 $lang['navigation_shops']           = '商店';
@@ -38,8 +37,7 @@ $lang['navigation_information_misc_siteUptime']       = '狀態頁';
 // Breadcrumbs
 $lang['navigation_breadcrumb_home']       = '家';
 $lang['navigation_breadcrumb_noFraud']    = '只沒有欺詐';
-$lang['navigation_breadcrumb_tor']        = '只 TOR 論壇';
-$lang['navigation_breadcrumb_i2p']        = '只 I2P 論壇';
+$lang['navigation_breadcrumb_torI2P']     = '只 TOR / I2P 論壇';
 $lang['navigation_breadcrumb_search']     = '搜索結果';
 $lang['navigation_breadcrumb_blogs']      = '安全和黑客博客';
 $lang['navigation_breadcrumb_shops']      = '商店';
@@ -68,7 +66,7 @@ $lang['sidebar_statistics_offline']   = '離線';
 $lang['sidebar_statistics_languages'] = '語言';
 $lang['sidebar_statistics_total']     = '整體';
 $lang['sidebar_advertisement']        = '廣告';
-$lang['sidebar_version']              = '版本 4.1';
+$lang['sidebar_version']              = '版本';
 $lang['sidebar_version_changelog']    = '更新日誌';
 // Link us site
 $lang['linkus_htmlCodeText']          = '獲取HTML代碼，將我們的橫幅嵌入您的網站。';
@@ -134,6 +132,7 @@ $lang['add_success_title']            = '成功!';
 $lang['add_success_text']             = '鏈接已添加。 感謝您的幫助.';
 $lang['add_fail_title']               = '錯誤!';
 $lang['add_fail_text']                = '發生錯誤。 請再試一次.';
+//$lang['add_fail_text_double']			  = 'There was an error. This site seems to be added already.';
 $lang['add_facebook_placeholder']     = '組名';
 // Hosting Site
 $lang['hosting_head']                 = '';
@@ -219,6 +218,8 @@ $lang['useful_pastebin']        = 'Pastebin';
 $lang['useful_security']        = '安全';
 $lang['useful_hackgame']        = '劈-遊戲';
 $lang['useful_serials']         = '序列號';
+$lang['useful_kodi']        	= 'Kodi';
+$lang['useful_jabber']        	= 'Jabber';
 // VPN Category
 $lang['vpn_paidvpn']            = '付費 VPN';
 $lang['vpn_vpninfo']            = 'VPN 信息';
@@ -257,82 +258,4 @@ $lang['out_redirectingIn']      = ' 在20秒內。';
 $lang['out_iframeText1']        = '您的瀏覽器無法顯示iframe:';
 $lang['out_iframeText2']        = '點擊此處顯示請求的頁面:';
 // FAQ site
-$lang['faq_head']               = 'Frequently Asked Questions';
-// Link
-$lang['faq_menu_link']                            = 'Link';
-$lang['faq_menu_link_question_linkStatusColors']  = 'What are the meanings of the different link status colors?';
-$lang['faq_menu_link_answer_linkStatusColors']    = '<span class="fa fa-circle" style="color:green"></span> <font color="black">online (nonfraud)</font></p>
-<p><span class="fa fa-circle" style="color:green"></span> <font color="lightgrey">online </font>(fraud)</p>
-<p><span class="fa fa-circle" style="color:orange"></span> maintenance / back online soon</p>
-<p><span class="fa fa-circle" style="color:red"></span> offline</p>
-<p><span class="fa fa-globe" style="color:green"></span> Tor</i></p>
-<p><span class="fa fa-star" style="color:red"></span><font color="red"> <b>name</b> </font> Advertisement<p>
-<p><span class="fa fa-star" style="color:red"></span> Partner or banner backlink</p>
-<p><span class="fa fa-star" style="color:green"></span> Partner or backlink';
-$lang['faq_menu_link_question_linkOffline']       = 'A link is offline';
-$lang['faq_menu_link_answer_linkOffline']         = 'If there is a link which seems to be offline, you have the ability to report this site.</p>
-<p>We will review your reported link and mark the site as offline';
-$lang['faq_menu_link_question_addedLink']         = 'I submitted a link, but it is still not visible';
-$lang['faq_menu_link_answer_addedLink']           = 'We verify every submitted link within one week.</p>
-<p>If its still not visible, send us a DM via Twitter or contact us via jabber/icq.';
-$lang['faq_menu_link_question_socialData']       = 'How to get social data?';
-$lang['faq_menu_link_answer_socialData']         = 'If you open a forum link, you may get additional information in the headers bar.</p>
-<p>Complete informations you can get on the <a href="search.php">search</a> site.';
-// Privacy
-$lang['faq_menu_privacy']                            = 'Privacy';
-$lang['faq_menu_privacy_question_terms']             = 'Terms';
-$lang['faq_menu_privacy_answer_terms']               = 'By accessing the website at http://link-base.org,</p>
-<p>you are agreeing to be bound by these terms of service,</p>
-<p>all applicable laws and regulations,</p>
-<p>and agree that you are responsible for compliance with any applicable local laws.</p>
-<p>If you do not agree with any of these terms, you are prohibited from using or accessing this site.</p>
-<p>The materials contained in this website are protected by applicable copyright and trademark law.';
-$lang['faq_menu_privacy_question_useLicense']        = 'Use License';
-$lang['faq_menu_privacy_answer_useLicense']          = 'a) Permission is granted to temporarily download one copy of the materials
-(information or software) on LinkBaseORG\'s website for personal, non-commercial transitory viewing only.
-This is the grant of a license, not a transfer of title, and under this license you may not:</p>
-<p>I: modify or copy the materials;</p>
-<p>II: use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</p>
-<p>III: attempt to decompile or reverse engineer any software contained on LinkBaseORG\'s website;</p>
-<p>IV: remove any copyright or other proprietary notations from the materials; or</p>
-<p>V: transfer the materials to another person or "mirror" the materials on any other server.</p>
-<p>b) This license shall automatically terminate if you violate any of these restrictions and may be terminated by LinkBaseORG at any time.
-Upon terminating your viewing of these materials or upon the termination of this license,
-you must destroy any downloaded materials in your possession whether in electronic or printed format.';
-$lang['faq_menu_privacy_question_disclaimer']        = 'Disclaimer';
-$lang['faq_menu_privacy_answer_disclaimer']          = 'a) The materials on LinkBaseORG\'s website are provided on an \'as is\' basis. LinkBaseORG makes no warranties,
-expressed or implied, and hereby disclaims and negates all other warranties including, without limitation,
-implied warranties or conditions of merchantability, fitness for a particular purpose,
-or non-infringement of intellectual property or other violation of rights.</p>
-<p>b) Further, LinkBaseORG does not warrant or make any representations concerning the accuracy,
-likely results,
-or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.';
-$lang['faq_menu_privacy_question_limitations']       = 'Limitations';
-$lang['faq_menu_privacy_answer_limitations']         = 'In no event shall LinkBaseORG or its suppliers be liable for any damages (including, without limitation,
-damages for loss of data or profit,
-or due to business interruption) arising out of the use or inability to use the materials on LinkBaseORG\'s website,
-even if LinkBaseORG or a LinkBaseORG authorized representative has been notified orally or in writing of the possibility of such damage.
-Because some jurisdictions do not allow limitations on implied warranties,
-or limitations of liability for consequential or incidental damages, these limitations may not apply to you.';
-$lang['faq_menu_privacy_question_accuracy']          = 'Accuracy of materials';
-$lang['faq_menu_privacy_answer_accuracy']            = 'The materials appearing on LinkBaseORG website could include technical, typographical, or photographic errors.
-LinkBaseORG does not warrant that any of the materials on its website are accurate, complete or current.
-LinkBaseORG may make changes to the materials contained on its website at any time without notice.
-However LinkBaseORG does not make any commitment to update the materials.';
-$lang['faq_menu_privacy_question_links']             = 'Links';
-$lang['faq_menu_privacy_answer_links']               = 'LinkBaseORG has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site.
-The inclusion of any link does not imply endorsement by LinkBaseORG of the site.
-Use of any such linked website is at the user\'s own risk.';
-$lang['faq_menu_privacy_question_modifications']     = 'Modifications';
-$lang['faq_menu_privacy_answer_modifications']       = 'LinkBaseORG may revise these terms of service for its website at any time without notice.
-By using this website you are agreeing to be bound by the then current version of these terms of service.';
-$lang['faq_menu_privacy_question_governingLaw']      = 'Governing Law';
-$lang['faq_menu_privacy_answer_governingLaw']        = 'These terms and conditions are governed by and construed in accordance with the laws of Malaysia
-and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.';
-$lang['faq_menu_privacy_question_piwik']             = 'Matomo (previously Piwik)';
-$lang['faq_menu_privacy_answer_piwik']               = 'Disable user tracking via Piwik';
-// Advertisement
-$lang['faq_menu_advertisement']                      = 'Advertisement';
-$lang['faq_menu_advertisement_question_banner']      = 'Advertisement';
-$lang['faq_menu_advertisement_answer_banner']        = 'Big banner -> small banner -> custom banner';
 ?>
