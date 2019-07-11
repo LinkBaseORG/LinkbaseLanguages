307980bd5a02381a7f45f1de14558ed7903b4582
LinkBaseORG <38041853+LinkBaseORG@users.noreply.github.com>
2019-07-11 23:20:29 +0200

    Create lang.ka.php
    Georgian

---
+++ lang.ka.php
@@ -0,0 +1,260 @@
+<?php
+// Global
+$lang['site_error_backText']        = 'უკან';
+$lang['site_403_errorText']         = 'უკაცრავად, ამ საიტზე წვდომის უფლება არ გაქვთ.';
+$lang['site_404_errorText']         = 'უცებ! უკაცრავად, რომ გვერდი ვერ მოიძებნა.';
+$lang['site_500_errorText']         = 'უკაცრავად, შიდა შეცდომა იყო.';
+// Navigation
+$lang['navigation_toggle']          = 'გადართვა ნავიგაცია';
+$lang['navigation_home']            = 'მთავარი';
+// NavigationSub
+$lang['navigation_boards']          = 'დაფები';
+$lang['navigation_boards_all']      = 'ყველა დაფები';
+$lang['navigation_boards_noFraud']  = 'არა თაღლითობა';
+$lang['navigation_boards_torI2P']   = 'Tor / I2P მხოლოდ';
+// Navigation
+$lang['navigation_blogs']           = 'დღიურები';
+$lang['navigation_shops']           = 'აღაზიები';
+$lang['navigation_warez']           = 'Warez';
+$lang['navigation_useful']          = 'სასარგებლო';
+$lang['navigation_vpn']             = 'vPN';
+$lang['navigation_hosting']         = 'ჰოსტინგი';
+$lang['navigation_addLink']         = 'ლინკის დამატება';
+$lang['navigation_last5Added']      = 'ახლახანს 15 განახლებული ბმულის ჩვენება (RSS)';
+// NavigationSub
+$lang['navigation_information']                       = 'ინფორმაცია';
+$lang['navigation_information_contact']               = 'კონტაქტი';
+$lang['navigation_information_donate']                = 'შემოწირულობა';
+$lang['navigation_information_pricing']               = 'ფასები';
+$lang['navigation_information_linkUs']                = 'დაგვიკავშირდით';
+$lang['navigation_information_misc']                  = 'სხვადასხვა';
+$lang['navigation_information_misc_changelog']        = 'ხელმოწერა';
+$lang['navigation_information_misc_api']              = 'API';
+$lang['navigation_information_misc_faq']              = 'კითხვა';
+$lang['navigation_information_misc_rssAdded']         = 'RSS [დამატებულია]';
+$lang['navigation_information_misc_rssUpdated']       = 'RSS [განახლებული]';
+$lang['navigation_information_misc_siteUptime']       = 'საიტის ხანგრძლივობა';
+// Breadcrumbs
+$lang['navigation_breadcrumb_home']       = 'მთავარი';
+$lang['navigation_breadcrumb_noFraud']    = 'აჩვენე მხოლოდ არაფრაუნის დაფები';
+$lang['navigation_breadcrumb_torI2P']     = 'აჩვენე მხოლოდ TOR / I2P დაფები';
+$lang['navigation_breadcrumb_search']     = 'იების შედეგები';
+$lang['navigation_breadcrumb_blogs']      = 'უსაფრთხოება და გარჩევა ბლოგები';
+$lang['navigation_breadcrumb_shops']      = 'აღაზიები';
+$lang['navigation_breadcrumb_warez']      = 'Warez';
+$lang['navigation_breadcrumb_useful']     = 'სასარგებლო ბმულების ჩვენება';
+$lang['navigation_breadcrumb_vpn']        = 'VPN სერვისების ჩვენება';
+$lang['navigation_breadcrumb_hosting']    = 'ჰოსტინგის მომსახურება';
+$lang['navigation_breadcrumb_addLink']    = 'ახალი ლინკის დამატება';
+$lang['navigation_breadcrumb_reportLink'] = 'შეატყობინეთ მკვდარი ბმული';
+$lang['navigation_breadcrumb_contact']    = 'აგვიკავშირდით';
+$lang['navigation_breadcrumb_donate']     = 'შემოწირულობა';
+$lang['navigation_breadcrumb_pricing']    = 'რეკლამის დაგეგმვის გეგმები';
+$lang['navigation_breadcrumb_linkUs']     = 'დაგვიკავშირდით';
+$lang['navigation_breadcrumb_changelog']  = 'ვერსია changelog';
+$lang['navigation_breadcrumb_faq']        = 'კითხვა';
+// Subnavigation
+$lang['subnavigation_lastDbUpdate']       = 'ბოლო DB განახლება';
+// Sidebar
+$lang['sidebar_searchPlaceholder']    = 'ძიება';
+$lang['sidebar_linkQueue']            = 'ლინკები მდგომ';
+$lang['sidebar_linkQueue_added']      = 'დამატებული';
+$lang['sidebar_linkQueue_reported']   = 'იტყობინება';
+$lang['sidebar_statistics']           = 'სტატისტიკა';
+$lang['sidebar_statistics_online']    = 'ონლაინ';
+$lang['sidebar_statistics_offline']   = 'ფორუმზე';
+$lang['sidebar_statistics_languages'] = 'ენები';
+$lang['sidebar_statistics_total']     = 'საერთო ჯამში';
+$lang['sidebar_advertisement']        = 'რეკლამა';
+$lang['sidebar_version']              = 'ვერსია';
+$lang['sidebar_version_changelog']    = 'ხელმოწერა';
+// Link us site
+$lang['linkus_htmlCodeText']          = 'მიიღეთ კოდი თქვენს ვებ-გვერდზე ჩვენი ბანერის ჩასმა.';
+$lang['linkus_bbcodeText']            = 'მიიღეთ კოდი, ჩაწერეთ ჩვენი ბანერი ხელმოწერებში, მაგალითად ფორუმებში (MyBB, vBulletin, ...)';
+// Donate site
+$lang['donate_headText']              = 'შემოწირულობა, შეინახეთ ამ სერვისი ცოცხალი:';
+// Changelog site
+$lang['changelog_head']         = 'ხელმოწერა';
+$lang['changelog_committed']    = 'გამოაქვეყნა ახალი ვერსია განახლება.';
+// Pricing site
+$lang['pricing_smallHead']      = 'ატარა';
+$lang['pricing_smallInclude']   = 'მიიღეთ გამორჩეული "წითელი ვარსკვლავი';
+$lang['pricing_mediumHead']     = 'საშუალო';
+$lang['pricing_highlight']      = 'ყველაზე პოპულარული გეგმა';
+$lang['pricing_mediumInclude']  = '468px * 60px (+ წითელი ვარსკვლავი)';
+$lang['pricing_largeHead']      = 'დიდი';
+$lang['pricing_largeInclude']   = '920px * 120px (+ წითელი ვარსკვლავი)';
+$lang['pricing_perMonth']       = 'თვეში';
+$lang['pricing_contactUs']      = 'აგვიკავშირდით';
+// Search Site
+$lang['search_nameText']                = 'სახელი';
+$lang['search_typeHead']                = 'ტიპი';
+$lang['search_resetText']               = 'ძიების გადატვირთვა';
+$lang['search_category']                = 'კატეგორია';
+$lang['search_typeBoard']               = 'საბჭო';
+$lang['search_typeBlog']                = 'დღიური';
+$lang['search_typeWarez']               = 'Warez';
+$lang['search_typeVPN']                 = 'VPN';
+$lang['search_typeUseful']              = 'სასარგებლო';
+$lang['search_typeShop']                = 'აღაზია';
+$lang['search_typeHosting']             = 'ჰოსტინგი';
+// Contact us Site
+$lang['contact_donateText']             = 'შემოწირულობა, შეინახეთ ამ სერვისი ცოცხალი:';
+$lang['contact_contactInformationText'] = 'აკონტაქტო ინფორმაცია:';
+$lang['contact_twitterFollow']          = 'აყოლა';
+$lang['contact_warningHeader']          = 'გაფრთხილება!';
+$lang['contact_warningText']            = 'თუ გინდა დაგვიკავშირდით, <b> მხოლოდ </b> <b>ამ საიტის</b> შესახებ ნაჩვენები საკონტაქტო ინფორმაცია გამოიყენოთ, რათა თავიდან იქნას აცილებული შფოთვა!';
+// Report Link Site
+$lang['report_headText']              = 'ანგარიში';
+$lang['report_sitename']              = 'სახელი';
+$lang['report_url']                   = 'URL';
+$lang['report_reason']                = 'მიზეზი';
+$lang['report_reportBtn']             = 'შეატყობინე ბმულს!';
+$lang['report_success_title']         = 'წარმატება';
+$lang['report_success_text']          = 'ბმული იტყობინებოდა. მადლობა თქვენი დახმარებისთვის.';
+$lang['report_fail_title']            = 'გაფრთხილება!';
+$lang['report_fail_text']             = 'ეცდომა იყო. Გთხოვთ კიდევ სცადეთ.';
+$lang['report_fail_DoubleEntry_text'] = 'ეს საიტი უკვე მოხსენიებულია როგორც იტყობინება';
+
+// Add Link Site
+$lang['add_url']                      = 'URL';
+$lang['add_tor']                      = 'TOR URL';
+$lang['add_i2p']                      = 'I2P URL';
+$lang['add_facebook']                 = 'Facebook';
+$lang['add_twitter']                  = 'Twitter';
+$lang['add_category']                 = 'კატეგორია';
+$lang['add_languages']                = 'ენები';
+$lang['add_pleaseSelect']             = 'გთხოვთ აირჩიოთ';
+$lang['add_information']              = 'ინფორმაცია';
+$lang['add_information_placeholder']  = 'სარეზერვო დომენები, საკონტაქტო ინფორმაცია, ... (მაქსიმუმ 200 სიმბოლო)';
+$lang['add_addLink']                  = 'ლინკის დამატება';
+$lang['add_success_title']            = 'წარმატება!';
+$lang['add_success_text']             = 'ბმული დაემატა. მადლობა თქვენი დახმარებისთვის.';
+$lang['add_fail_title']               = 'გაფრთხილება!';
+$lang['add_fail_text']                = 'ეცდომა იყო. Გთხოვთ კიდევ სცადეთ.';
+$lang['add_fail_text_double'] 		  = 'ეცდომა იყო. ეს საიტი, როგორც ჩანს, უკვე დამატებულია.';
+$lang['add_facebook_placeholder']     = 'გუფის სახელი';
+// Hosting Site
+$lang['hosting_head']                 = 'ჰოსტინგი';
+$lang['hosting_service']              = 'სამსახური';
+$lang['hosting_creditcard']           = 'აკრედიტო ბარათი';
+$lang['hosting_locations']            = 'ადგილები';
+$lang['hosting_infoTextHead']         = 'ინფორმაცია';
+$lang['hosting_infoText']             = 'Norton იკვლევს საიდუმლო სამყაროს ტყვიაგაუმტარი ჰოსტინგი, რომელიც იმალება ღრმა მიწისქვეშა ბუნკერები,
+იზოლირებული ზღვაზე და ვრცელდება მთელს ვებზე. გაუქმება საფრთხეები, რომლებიც მოტყუება ამ მომსახურების, როგორიცაა botnets,
+მავნე, ransomware და შავი ბაზარი და ისწავლე, როგორ დავიცვათ თავი "ყველაზე საშიში ქალაქი ინტერნეტში - სადაც კიბერდანაშაული მიდის დამალვა"';
+$lang['hosting_infoStartVideo']       = 'ვიდეოს დაწყება';
+// Footer
+$lang['footer_generatedInStart']      = 'გვერდი გენერირებული ';
+$lang['footer_generatedInEnd']        = ' წამი';
+// Language Category
+$lang['promoted']        = '';
+$lang['partner']         = 'პარტნიორი';
+$lang['al']              = 'ალბანური';
+$lang['ar']              = 'არაბული';
+$lang['az']              = 'აზერბაიჯანი';
+$lang['bs']              = 'ბოსნიელი';
+$lang['zh']              = 'ჩინური';
+$lang['hr']              = 'ხორვატია';
+$lang['cz']              = 'ჩეხური';
+$lang['da']              = 'დანიური';
+$lang['nl']              = 'ჰოლანდიური';
+$lang['en']              = 'ინგლისური';
+$lang['fr']              = 'ფრანგული';
+$lang['ge']              = 'ქართული';
+$lang['de']              = 'გერმანული';
+$lang['gr']              = 'ბერძნული';
+$lang['id']              = 'ინდონეზიელი';
+$lang['it']              = 'იტალიური';
+$lang['jp']              = 'იაპონელი';
+$lang['my']              = 'მალაიზიელი';
+$lang['fas']             = 'სპარსული';
+$lang['pl']              = 'პოლონური';
+$lang['pt']              = 'პორტუგალიური';
+$lang['ro']              = 'რუმინეთი';
+$lang['ru']              = 'რუსული';
+$lang['rs']              = 'სერბული';
+$lang['es']              = 'ესპანური';
+$lang['th']              = 'ტაილანდური';
+$lang['tr']              = 'თურქული';
+$lang['ua']              = 'უკრაინული';
+$lang['uz']              = 'უზბეკ';
+$lang['vn']              = 'ვიეტნამური';
+// Shop Category
+$lang['shops_ccv']              = '';
+$lang['shops_accounts']         = '';
+$lang['shops_icq']              = '';
+$lang['shops_drugs']            = '';
+$lang['shops_rdp']              = '';
+$lang['shops_flood']            = '';
+$lang['shops_exploit']          = '';
+$lang['shops_cccheck']          = '';
+$lang['shops_iptv']             = '';
+$lang['shops_other']            = '';
+// Useful Category
+$lang['useful_vuln']            = '';
+$lang['useful_unsorted']        = '';
+$lang['useful_ffaddons']        = '';
+$lang['useful_cryptography']    = '';
+$lang['useful_scanner']         = '';
+$lang['useful_antimalware']     = '';
+$lang['useful_linklist']        = '';
+$lang['useful_multiuploader']   = '';
+$lang['useful_exchanger']       = '';
+$lang['useful_trashmail']       = '';
+$lang['useful_challenges']      = '';
+$lang['useful_securemail']      = '';
+$lang['useful_cardsharing']     = '';
+$lang['useful_toplist']         = '';
+$lang['useful_nfo']             = '';
+$lang['useful_ddosprotect']     = '';
+$lang['useful_cryptocurrency']  = '';
+$lang['useful_documentation']   = '';
+$lang['useful_linkcrypter']     = '';
+$lang['useful_startpage']       = '';
+$lang['useful_telegramgrp']     = '';
+$lang['useful_uptimechecker']   = '';
+$lang['useful_pastebin']        = '';
+$lang['useful_security']        = '';
+$lang['useful_hackgame']        = '';
+$lang['useful_serials']         = '';
+$lang['useful_kodi']        	= '';
+$lang['useful_jabber']        	= '';
+// VPN Category
+$lang['vpn_paidvpn']            = '';
+$lang['vpn_vpninfo']            = '';
+$lang['vpn_freevpn']            = '';
+$lang['vpn_socks']              = '';
+$lang['vpn_webproxy']           = '';
+$lang['vpn_checker']            = '';
+// Warez Category
+$lang['warez_boards']           = '';
+$lang['warez_anime']            = '';
+$lang['warez_tvstream']         = '';
+$lang['warez_ftp']              = '';
+$lang['warez_documentation']    = '';
+$lang['warez_ebook']            = '';
+$lang['warez_xdcc']             = '';
+$lang['warez_music']            = '';
+$lang['warez_games']            = '';
+$lang['warez_movies']           = '';
+$lang['warez_mixed']            = '';
+$lang['warez_android']          = '';
+$lang['warez_apple']            = '';
+$lang['warez_moviestreams']     = '';
+$lang['warez_search']           = '';
+$lang['warez_porn']             = '';
+$lang['warez_usenet']           = '';
+$lang['warez_scripts']          = '';
+$lang['warez_torrent']          = '';
+// Board Redirection Site
+$lang['out_home']               = '';
+$lang['out_report']             = '';
+$lang['out_voteForUs']          = '';
+$lang['out_vote']               = '';
+$lang['out_closeFrame']         = '';
+$lang['out_redirectingTo']      = '';
+$lang['out_redirectingIn']      = '';
+$lang['out_iframeText1']        = '';
+$lang['out_iframeText2']        = '';
+?>
